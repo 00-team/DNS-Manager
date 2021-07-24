0e64481740af8a091218c31fea195d1f71b36782
i007c <dr007cc@gmail.com>
2021-07-24 08:30:01 +0430

    block some trash content for save

--- src/redux/actions/saved-dns/save-saved-dns.js
+++ src/redux/actions/saved-dns/save-saved-dns.js
@@ -10,9 +10,14 @@ import {
 } from '../base/types'
 
 
-export const SaveSavedDns = (name, dns1, dns2) => (dispatch) => {
+export const SaveSavedDns = (dnsName, dns1, dns2) => (dispatch) => {
     const db = new sqlite.Database('db.sqlite3')
 
+    if (!dns1 || !dns2 || !dnsName) return dispatch({ type: ERROR_ALERT, payload: 'Please fill in all fields' })
+    if (dns1.length < 6 || dns1.length < 6 || dnsName.length < 1) {
+        return dispatch({ type: ERROR_ALERT, payload: 'Please fill in all fields' })
+    }
+    
     db.serialize(() => {
         let lastID = 1;
 
@@ -22,10 +27,11 @@ export const SaveSavedDns = (name, dns1, dns2) => (dispatch) => {
             if (rows) if (rows.length > 0) lastID = rows[0].id + 1
         })
 
-        db.run(`INSERT INTO saved_dns VALUES (${lastID}, '${name}', '${dns1}', '${dns2}')`, (err) => {
+        db.run(`INSERT INTO saved_dns VALUES (${lastID}, '${dnsName}', '${dns1}', '${dns2}')`, (err) => {
             if (err) console.log(err)
         })
 
         db.close(() => dispatch(loadSavedDns()))
     })
+
 }
