ebf0195147ac34ed9d46addc1684050a5bdde7af
i007c <dr007cc@gmail.com>
2021-07-25 23:32:02 +0430

    init sidebar

---
+++ src/components/layouts/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './sass/sidebar.scss'
+
+const SideBar = () => {
+    return (
+        <div className='sidebar-container'>
+            <div className='sidebar'>
+
+            </div>
+        </div>
+    )
+}
+
+export default SideBar
