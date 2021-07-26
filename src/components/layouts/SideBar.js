00a0f5df3921e908c0ce03343c3e0171e5d35f2b
i007c <dr007cc@gmail.com>
2021-07-26 07:59:31 +0430

    add some actions and icons

--- src/components/layouts/SideBar.js
+++ src/components/layouts/SideBar.js
@@ -1,12 +1,42 @@
 import React from 'react'
 
+// redux
+import { useDispatch, useSelector } from 'react-redux'
+
+// icons
+import { FiDatabase, FiServer, FiSettings } from 'react-icons/fi'
+import TeamIcon from '../common/TeamIcon'
+
+// style
 import './sass/sidebar.scss'
 
 const SideBar = () => {
+    const dispatch = useDispatch()
+    const sidebarState = useSelector(state => state.sidebar)
+
+    console.log(sidebarState);
+
     return (
         <div className='sidebar-container'>
             <div className='sidebar'>
+                <div className='top-level'>
+                    <div className='action dns-changer selected'>
+                        <FiServer />
+                    </div>
 
+                    <div className='action dns-database'>
+                        <FiDatabase />
+                    </div>
+                </div>
+
+                <div className="bottom-level">
+                    <div className="action about">
+                        <TeamIcon />
+                    </div>
+                    <div className='action settings'>
+                        <FiSettings />
+                    </div>
+                </div>
             </div>
         </div>
     )
