d5f43248d2de30539ceb67c8bcfa57f502494a54
i007c <dr007cc@gmail.com>
2021-07-31 00:25:49 +0430

    fix editor tabs problems like scroll and text overflows

--- src/components/editor/Tabs.js
+++ src/components/editor/Tabs.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 // style
 import './sass/tabs.scss'
 
 
 const Tabs = ({ TabList, SetCurrentTab }) => {
+    const tabsRef = useRef(null)
     const EmptyTabs = <div className='empty-tabs'> There is no Tab to show </div>
     const TabsList = TabList.map((tab, tabIndex) => 
         <Tab 
@@ -17,7 +18,7 @@ const Tabs = ({ TabList, SetCurrentTab }) => {
 
     return (
         <div className='tabs-container'>
-            <div className='tabs'>
+            <div className='tabs' onWheel={e => { if (tabsRef.current) tabsRef.current.scrollLeft += e.deltaY }} ref={tabsRef}>
                 {TabList.length < 1 ? EmptyTabs : TabsList}
             </div>
         </div>
@@ -33,7 +34,7 @@ const Tab = ({ isSelected, onClick, TabName }) => {
     return (
         <div className={'tab-container' + (isSelected ? ' selected' : '')} onClick={onClick} >
             <div className='tab'>
-                {TabName}
+                <span>{TabName}</span>
             </div>
         </div>
     )
