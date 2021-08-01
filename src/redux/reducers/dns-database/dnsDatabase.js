69cc9a8400c2405dc1dd49bb41388388dfd411da
i007c <dr007cc@gmail.com>
2021-08-01 23:27:05 +0430

    init redux for dns database

---
+++ src/redux/reducers/dns-database/dnsDatabase.js
@@ -0,0 +1,30 @@
+import { 
+    LOAD_DNS_DATABASE,
+    LOADING_DNS_DATABASE,
+} from "./types";
+
+const initState = {
+    dnsList: [
+        { dnsName: 'Google', dns1: '8.8.8.8', dns2: '8.4.4.8' },
+        { dnsName: 'Cloudflare', dns1: '8.8.8.8', dns2: '8.4.4.8' },
+        { dnsName: 'Random', dns1: '7.7.7.7', dns2: '7.6.6.7' },
+    ],
+    loading: false
+}
+
+export default (state=initState, action) => {
+    switch (action.type) {
+        case LOADING_DNS_DATABASE:
+            return {
+                ...state,
+                loading: action.payload
+            }
+        case LOAD_DNS_DATABASE:
+            return {
+                ...state,
+                dnsList: action.payload
+            }
+        default:
+            return state;
+    }
+}
