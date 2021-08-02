31e91cb711cc1dc36058d82eb4187805a2a6da47
i007c <dr007cc@gmail.com>
2021-08-03 00:16:01 +0430

    make alerts redux

---
+++ src/redux/reducers/base/types.js
@@ -0,0 +1,3 @@
+export const INFO_ALERT = 'INFO_ALERT';
+export const ERROR_ALERT = 'ERROR_ALERT';
+export const SUCCESS_ALERT = 'SUCCESS_ALERT';
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -7,9 +7,12 @@ import dnsChanger from './dns-changer/dnsChanger';
 import dnsDatabase from './dns-database/dnsDatabase';
 import settings from './settings/settings';
 
+// base
+import alerts from './base/alerts'
 
 export default combineReducers({
     sidebar,
+    alerts,
     DnsChanger: dnsChanger,
     DnsDatabase: dnsDatabase,
     Settings: settings
