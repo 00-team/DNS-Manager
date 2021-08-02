f48cd53171901ec20f1937d76e355e5145ec9b15
i007c <dr007cc@gmail.com>
2021-08-02 22:44:25 +0430

    init settings redux

---
+++ src/redux/reducers/settings/settings.js
@@ -0,0 +1,28 @@
+import { CHANGE_SETTINGS_TAB } from "./types"
+
+const initState = {
+    tabs: [
+        {
+            id: 1,
+            tabName: 'Settings',
+            isSelected: true
+        },
+        {
+            id: 2,
+            tabName: 'About Us',
+            isSelected: false
+        },
+    ]
+}
+
+export default (state=initState, action) => {
+    switch (action.type) {
+        case CHANGE_SETTINGS_TAB:
+            return {
+                ...state,
+                tabs: action.payload
+            }
+        default:
+            return state;
+    }
+}
