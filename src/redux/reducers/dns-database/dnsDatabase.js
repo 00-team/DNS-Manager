9ece2f98f34d74f9539d2d1957d4ee1a752e32c1
i007c <dr007cc@gmail.com>
2021-08-03 23:02:57 +0430

    make dns data base action

--- src/redux/reducers/dns-database/dnsDatabase.js
+++ src/redux/reducers/dns-database/dnsDatabase.js
@@ -4,12 +4,8 @@ import {
 } from "./types";
 
 const initState = {
-    dnsList: [
-        { id: 1, tabName: 'Google', dns1: '8.8.8.8', dns2: '8.4.4.8', isSelected: true },
-        { id: 2, tabName: 'Cloudflare', dns1: '1.1.1.1', dns2: '1.0.0.1', isSelected: false },
-        { id: 3, tabName: 'Random', dns1: '7.7.7.7', dns2: '7.6.6.7', isSelected: false },
-    ],
-    loading: false
+    dnsList: [],
+    loading: true,
 }
 
 export default (state=initState, action) => {
