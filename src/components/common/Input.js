d89ab0bff34c78d8ac7c30a7439e638f8a102fc7
i007c <dr007cc@gmail.com>
2021-08-03 23:24:23 +0430

    finish input for now

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const Input = () => {
+// style
+import './sass/input.scss'
+
+const Input = ({ onChange, placeholder, defaultValue, customStyle }) => {
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.value = defaultValue
+        }
+    }, [defaultValue, inputRef])
+
     return (
-        <div>
-            
+        <div className='input-container'>
+            <input 
+                ref={inputRef}
+                style={customStyle}
+                type="text" 
+                className='input'
+                placeholder={placeholder} 
+                onChange={e => onChange(e.target.value)}
+                defaultValue={defaultValue}
+            />
         </div>
     )
+}
+
+Input.defaultProps = {
+    onChange: () => {},
+    placeholder: '',
+    defaultValue: '',
+    customStyle: {},
 }
 
 export default Input
