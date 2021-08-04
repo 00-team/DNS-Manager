4a1515387ce30c705efe9ae69ef40cf443b7d99e
i007c <dr007cc@gmail.com>
2021-08-05 02:12:15 +0430

    change init page to dns-changer

--- src/redux/reducers/sidebar/sidebar.js
+++ src/redux/reducers/sidebar/sidebar.js
@@ -3,7 +3,7 @@ import { CHANGE_PAGE } from "./types"
 const allowPages = ['dns-changer', 'dns-database', 'settings', 'about']
 
 const initState = {
-    page: 'about',
+    page: 'dns-changer',
 }
 
 export default function (state = initState, action) {
